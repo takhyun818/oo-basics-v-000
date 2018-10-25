61f4635c59913ba26711d4635ed7a45df7d4ac2c
Tak H Kim <takhyun818@gmail.com>
2018-10-25 22:54:25 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -40,13 +40,13 @@ end
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
- 
+
   def initialize(title)
     @title = title
   end
- 
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
- 
+
 end
